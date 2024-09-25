3a2d495ebdaec119aa420eae75901b912bf8ab6c
Mitarumi <dipovictor07@gmail.com>
2024-09-25 12:36:57 +0100

    b

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,8 +11,8 @@ def number_of_subscribers(subreddit):
     if subreddit is None or type(subreddit) is not str:
         return 0
     else:
-        r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                        headers={'User-Agent': 'Python/requests:APIproject:\
+        r=requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                         headers={'User-Agent': 'Python/requests:APIproject:\
     v1.0.0 (by /u/aaorrico23)'}).json()
         subs = r.get("data", {}).get("subscribers", 0)
         return subs
